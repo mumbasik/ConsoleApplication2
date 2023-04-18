1d109819a0bd785f202242dd2104953d99cf8ba6
zadrot <zadrot@DESKTOP-L5ER55A>
2023-04-19 02:29:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,98 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //N1!
+    cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n";
+
+    cout << "\t\t\t\t\"\Calendar\"\ \n\n";
+
+    cout << "\t\t\t    \t  2023\n\n";
+
+    cout << "\t\t\t    \t  April\n\n";
+
+    cout << "\tMN\tTS\tWD\tTRS\tFRD\tSTD\tSND\n\n";
+
+    cout << "\t\t\t\t\t\t 1\t 2\n\n";
+
+    cout << "\t3\t4\t5\t 6\t 7\t 8 \t 9\n\n";
+
+    cout << "\t10\t11\t12\t 13\t 14\t 15\t 16\n\n";
+
+    cout << "\t17\t18\t19\t 20\t 21\t 22\t 23\n\n";
+
+    cout << "\t24\t25\t26\t 27\t 28\t 29\t 30\n\n";
+
+    cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n";
+
+    //N2!
+    cout << "\t\t     /| \n;";
+    cout << "\t\t___ / | \n";
+    cout << "\t\t| |/  |  \n";
+    cout << "\t\t| /   |  \n";
+    cout << "\t\t|/    |  \n";
+    cout << "\t\t/_____|     \n";
+    cout << "\t\t| ___ |\n";
+    cout << "\t\t| |_| |\n";
+    cout << "\t\t| ___ |\n";
+    cout << "\t\t| | | |---------\n";
+    cout << "\t\t| |.| || | | | | \n";
+    cout << "\t\t| |_| || | | | | \n";
+    cout << "\t\t|     |---------  \n\n\n";
+
+    //N3!
+    cout << " ******   **              ****   ****      ****          * \n";             
+    cout << " ******   **              ****   ****      ****          * \n";
+    cout << " **  **   **            **  **   ** **    ** **         * * \n";
+    cout << " **  **   **          **    **   **  **  **  **        *   * \n";
+    cout << " **  **   **        **      **   **    **    **       *     * \n";
+    cout << " ******   **      **        **   **          **      ********* \n";
+    cout << " ******   **    **          **   **          **     **      ** \n"; 
+    cout << "**    **  **  **            **   **          **    **        ** \n";
+    cout << "**    **  ****              **   **          **   **          ** \n";
+    cout << "**    **  ****              **   **          **  **            ** \n\n";
+
+    //N4!
+    cout << " *******\n";
+    cout << "        ***\n";
+    cout << "           ***\n";
+    cout << "             ***\n";
+    cout << "                **\n";
+    cout << "                   **\n";
+    cout << "                     ***\n";
+    cout << "                       **********\n";
+    cout << "                       *       *\n";
+    cout << "                        *     *\n";
+    cout << "                         *  **\n";
+    cout << "                          ***\n\n";
+
+    //N5!
+    
+    cout << "<<Dmytriy Zhuravskiy>>\n";
+    cout << "Professional worker at Microsoft Corporation\n";
+    cout << "USA, Washington\n\n";
+    cout << "General Information\n\n";
+    cout << "Microsoft software developer, game designer\n\n";
+    cout << "Experience of work\n";
+    cout << "For 8 years I'm working(2015, 8 years(specialist))\n\n";
+    cout << "Education\n";
+    cout << "Received higher education in IT company \"\Step\"\(Ukraine,Odessa) ";
+
+
+
+
+
+
+
+
+
+        
+
+
+    
+}
+
